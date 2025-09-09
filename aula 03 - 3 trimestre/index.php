d624c0f5afb8c99d1638e44e68c0a209bb128fab
YureC2008 <yure.csouza2@aluno.edu.es.gov.br>
2025-09-09 11:57:15 -0300

    index.php

---
+++ aula 03 - 3 trimestre/index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title> Aula 03 - PHP </title>
+    </head>
+    <body>
+       <?php
+            $nome1="Warley";
+            $nome2="Yure";
+
+            $n1=10;
+            $n2=2;
+
+            $soma=$n1+$n2;
+
+            echo "Meu nome é " .$nome1 ." e minha dupla é " .$nome2;
+            echo "<br>Meu nome é $nome1 e minha dupla é $nome2";
+
+            echo "<br><br>A soma de $n1 com $n2 é igual a $soma.";
+       ?>
+    </body>
+</html>        
