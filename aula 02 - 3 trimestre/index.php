73579df4286c5342463f158a9e8114226b4d74a6
YureC2008 <yure.csouza2@aluno.edu.es.gov.br>
2025-09-10 11:16:49 -0300

    index.php

---
+++ aula 02 - 3 trimestre/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title> Aula 02 - PHP </title>
+    </head>
+    <body>
+       <?php
+            $nome1="Warley";
+            $nome2="yure";
+
+            echo "Meu nome é " .$nome1 ."e minha dupla é " .$nome2;
+       ?>
+    </body>
+</html>        
