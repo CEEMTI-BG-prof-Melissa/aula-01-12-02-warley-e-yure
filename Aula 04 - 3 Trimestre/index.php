b6747addabe1b30a2c4ff3b9b1ffe9de39b50ee1
YureC2008 <yure.csouza2@aluno.edu.es.gov.br>
2025-09-30 11:16:42 -0300

    Create index.php

---
+++ Aula 04 - 3 Trimestre/index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title> Aula 04 - PHP </title>
+    </head>
+    <body>
+        <?php
+           $n1=$_GET["a"];
+           $n2=$_GET["b"];
+           
+           $soma=($n1+$n2);
+
+           echo "A soma dos números $n1 e $n2 é igual á $soma";
+
+        ?>
+    </body>
+</html>    
